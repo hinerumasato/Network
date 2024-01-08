b68638b83d92f1ae6eb7becd59888752fd7a128e
masato <thangloitran406@gmail.com>
2024-01-09 00:49:15 +0700

    Fix problem in RAF socket Service

--- src/Teach/Exams/Exam1/Server/Service.java
+++ src/Teach/Exams/Exam1/Server/Service.java
@@ -36,6 +36,23 @@ public class Service {
         return instance;
     }
 
+    private char[] toCharArray(String str, int length) {
+        char[] strChars = new char[length];
+        str.getChars(0, str.length(), strChars, 0);
+        return strChars;
+    }
+
+    private byte[] charToByte(char[] chars) {
+        byte[] bytes = new byte[chars.length];
+        for(int i = 0; i < bytes.length; i++)
+            bytes[i] = (byte)chars[i];
+        return bytes;
+    }
+
+    private String bytesToString(byte[] bytes) {
+        return new String(bytes);
+    }
+
     public void insertCandidate(Candidate candidate) throws IOException {
         // Ghi đè lại size của header
         raf.seek(0);
@@ -48,12 +65,17 @@ public class Service {
 
         candidate.setId(size);
 
+        // Dịch string ra byte[]
+        byte[] nameBytes = charToByte(toCharArray(candidate.getName(), 25));
+        byte[] birthdayBytes = charToByte(toCharArray(candidate.getBirthday(), 10));
+        byte[] addressBytes = charToByte(toCharArray(candidate.getAddress(), 25));
+
         // Ghi thông tin thí sinh
         long pointer = raf.getFilePointer();
         raf.writeInt(candidate.getId());
-        raf.writeUTF(candidate.getName());
-        raf.writeUTF(candidate.getBirthday());
-        raf.writeUTF(candidate.getAddress());
+        raf.write(nameBytes);
+        raf.write(birthdayBytes);
+        raf.write(addressBytes);
         byte[] bytes = new byte[100000];
         raf.write(bytes);
 
@@ -62,38 +84,17 @@ public class Service {
 
     public void writeFile(int id, byte[] buffer) throws IOException {
         long pointer = map.get(id);
+        pointer += 4 + 25 + 10 + 25;
         raf.seek(pointer);
-
-        raf.readInt();
-        raf.readUTF();
-        raf.readUTF();
-        raf.readUTF();
-
         raf.write(buffer);
     }
 
     public void update(int id, String address) throws IOException {
         long pointer = map.get(id);
+        pointer += 4 + 25 + 10;
         raf.seek(pointer);
-        
-        int _id = raf.readInt();
-        String name = raf.readUTF();
-        String birthday = raf.readUTF();
-        String oldAddress = raf.readUTF();
-
-        byte[] bytes = new byte[100000];
-        raf.read(bytes);
-
-        Candidate candidate = new Candidate(name, birthday, address);
-        candidate.setId(id);
-
-        // Ghi dữ liệu mới
-        raf.seek(pointer);
-        raf.writeInt(candidate.getId());
-        raf.writeUTF(candidate.getName());
-        raf.writeUTF(candidate.getBirthday());
-        raf.writeUTF(candidate.getAddress());
-        raf.write(bytes);
+        byte[] addressBytes = charToByte(toCharArray(address, 25));
+        raf.write(addressBytes);
 
     }
 
@@ -105,11 +106,14 @@ public class Service {
         long pointer = map.get(id);
         raf.seek(pointer);
 
-        int _id = raf.readInt();
-        String name = raf.readUTF();
-        String birthday = raf.readUTF();
-        String address = raf.readUTF();
+        byte[] nameBytes = new byte[25];
+        byte[] birthdayBytes = new byte[10];
+        byte[] addressBytes = new byte[25];
 
+        int _id = raf.readInt();
+        raf.read(nameBytes);
+        raf.read(birthdayBytes);
+        raf.read(addressBytes);
         byte[] bytes = new byte[100000];
         raf.read(bytes);
         int size = 0;
@@ -117,6 +121,9 @@ public class Service {
             if(bytes[i] != 0)
                 size++;
             else break;
+        String name = bytesToString(nameBytes);
+        String birthday = bytesToString(birthdayBytes);
+        String address = bytesToString(addressBytes);
         return "ID = " + _id + ", NAME = " + name + ", BIRTHDAY = " + birthday + ", ADDRESS = " + address + ", FILE SIZE = " + size;
     }
 }
